caec43e1a1094614805a47445c22955d4fb3f39d
Rimo <rimo1031.dev@gmail.com>
2022-08-25 02:44:29 +0900

    빙고 사이즈 조절 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,101 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 
-const Select_Button = (
-  { name, code }: { name: string; code: number },
-  setGame: (game: number) => void
-) => {
+const Radio = ({
+  name,
+  value,
+  state,
+  setState
+}: {
+  name: string;
+  value: number;
+  state: number;
+  setState: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+  const handleRadioClick = (size: number) => {
+    setState(size);
+  };
   return (
     <label htmlFor={name}>
-      <input key={name} type="radio" value={code} id={name} />
+      <input
+        key={name}
+        type="radio"
+        value={value}
+        id={name}
+        checked={state === value}
+        onClick={() => handleRadioClick(value)}
+      />
       {name}
     </label>
   );
 };
 
+const Select_Size = ({
+  size,
+  setSize
+}: {
+  size: number;
+  setSize: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+  const BINGO_SIZE = [3, 4, 5, 6, 7];
+  return (
+    <div>
+      <text>빙고 크기 선택: </text>
+      {BINGO_SIZE.map((bingo_size) =>
+        Radio({ name: bingo_size.toString(), value: bingo_size, state: size, setState: setSize })
+      )}
+    </div>
+  );
+};
+
 const Select_Game = () => {
-  const [game, setGame] = useState(0);
-  const GAME_LIST = [
-    { name: 'SDVX', code: 0 },
-    { name: 'IIDX', code: 1 },
-    { name: 'CHUNITHM', code: 2 }
-  ];
-  return <div>{GAME_LIST.map((game) => Select_Button(game, setGame))}</div>;
+  const GAME_LIST = [{ name: 'SDVX', value: 0 }];
+  return (
+    <div>
+      <text>기종 선택: </text>
+      {GAME_LIST.map((game) => (
+        <label htmlFor={game.name}>
+          <input key={game.name} type="radio" value={game.value} id={game.name} />
+          {game.name}
+        </label>
+      ))}
+    </div>
+  );
 };
 
 const Bingo_Cell = () => {
   return (
     <td>
-      <text>Hello, world!</text>
+      <text>곡명:</text>
       <br></br>
-      <text>Select Song:</text>
+      <text>달성 조건:</text>
     </td>
   );
 };
 
-const Bingo_Table = () => {
-  const arr: number[] = [1, 2, 3];
+const Bingo_Table = ({ state }: { state: number }) => {
+  const arr: number[] = [];
+  for (let i: number = 1; i <= state; i++) {
+    arr.push(i);
+  }
   return (
     <table>
-      {arr.map(() => (
-        <tr>{arr.map(() => Bingo_Cell())}</tr>
-      ))}
+      <tbody>
+        {arr.map(() => (
+          <tr>{arr.map(() => Bingo_Cell())}</tr>
+        ))}
+      </tbody>
     </table>
   );
 };
 
 const App = () => {
+  const [size, setSize] = useState(0);
   return (
     <div className="App">
+      <Select_Size size={size} setSize={setSize} />
       <Select_Game />
-      <Bingo_Table />
+      <Bingo_Table state={size} />
     </div>
   );
 };
